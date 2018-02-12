8f1ffb7ab0f9684527d1f816abe2ef17e0668e2f
Antonio Villamarin <anviusr@gmail.com>
2018-02-12 10:38:45 +0100

    Get from Array

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 window.onload = function() {
   var values = fetch("https://api.coinmarketcap.com/v1/ticker/"+coin.crypto+"/?convert=EUR")
                 .then(function(response) { return response.json(); })
-                .then(function(data) { coin = Object.assign(coin, data) });
+                .then(function(data) { coin = Object.assign(coin, data[0]) });
 }
 
 console.log(coin);
