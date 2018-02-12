ebc193b995dbefe4314850b6ef66569c986694dc
Antonio Villamarin <anviusr@gmail.com>
2018-02-12 21:11:16 +0100

    Indice array get

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 window.onload = function() {
   // Configuracion
   var crypto = "cardano";
-  
+
   fetch(`https://api.coinmarketcap.com/v1/ticker/${crypto}/?convert=EUR`)
-  .then(function(response) { return response.json(); })
+  .then(function(response) { return response.json()[0]; })
   .then(function(data) {
     document.title.value = data.name;
     var title = document.getElementsByTagName("h1")[0];
