228012fa394edffda7e697f728012cdb32a98c49
Antonio Villamarin <anviusr@gmail.com>
2018-02-12 10:32:08 +0100

    Testing

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 window.onload = function() {
   var values = fetch("https://api.coinmarketcap.com/v1/ticker/"+coin.name+"/?convert=EUR")
-                .then(function(response) { return response.json(); })
+                .then(function(response) { console.log(response); return response.json(); })
                 .then(function(data) { coin = data; });
 }
 
